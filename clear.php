abead13c3596f949f3826a8f8cedf06bde6d8d60
Dineth Pramodya <74010757+dinethlive@users.noreply.github.com>
2022-06-01 20:27:10 +0530

    Updated clear.php

--- clear.php
+++ clear.php
@@ -1,12 +1,5 @@
 <?php 
-
-//create a file handler by opening the file
 $myTextFileHandler = @fopen("log.html","r+");
-
-//truncate the file to zero
-//or you could have used the write method and written nothing to it
 @ftruncate($myTextFileHandler, 0);
-
-//use location header to go back to index.html
 header("Location:index.php");
  ?>
